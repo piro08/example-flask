c2bdc5562bfd8880c8a1df68dffbe8737c5c2939
piro08 <hirotokatsudonsub@gmail.com>
2025-07-04 18:24:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from threading import Thread
+import tw_code
+
+Thread(target=tw_code.run).start()
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb'
+    return 'pong'
 
 
 if __name__ == "__main__":
